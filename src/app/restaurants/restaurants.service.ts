8f0003d2fc3d154d8cd64d84c3aafc5f2f37cbd5
yagoliveira92 <yagoliveira92@gmail.com>
2019-01-29 12:47:28 -0300

    Create to mock service

---
+++ src/app/restaurants/restaurants.service.ts
@@ -0,0 +1,17 @@
+import {Restaurant} from './restaurant/restaurant.model';
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
+import {MEAT_API} from '../app.api';
+import {Observable} from 'rxjs';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class RestaurantsService {
+
+  constructor(private http: Http) {}
+
+  restaurants(): Observable<Restaurant[]> {
+    return this.http.get(`${MEAT_API}/restaurants`)
+      .map(response => response.json());
+  }
+}
